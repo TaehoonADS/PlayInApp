4d322459db584e82f0d500e37da8e4954a34b3c3
kangt2000 <kangt2000@kangt2000ui-Macintosh.local>
2016-04-26 17:46:18 +0900

    add comments

--- app/src/main/java/com/ninestairs/playinapp/MainActivity.java
+++ app/src/main/java/com/ninestairs/playinapp/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
         //물건 구입 요청한다
         //물건 요청 되면 새로운 기능 추가한다
         //onStart 에서 산것 기록 없애고 DB에 넣어야 함 (왜냐하면 환불 후 또 구입하는 경우 가능하게 하려면
-        //
+        //새로운 커멘트 추가
+        //다시 추가
 
     }
 }
