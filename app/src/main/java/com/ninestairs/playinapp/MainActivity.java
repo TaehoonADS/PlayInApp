995dc55f8c8e9de2cb227e4cc0cbda6f6599646d
kangt2000 <kangt2000@kangt2000ui-Macintosh.local>
2016-04-26 17:33:54 +0900

    Initial commit

---
+++ app/src/main/java/com/ninestairs/playinapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ninestairs.playinapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //물건 목록 들고온다
+        //물건 구입 요청한다
+        //물건 요청 되면 새로운 기능 추가한다
+        //onStart 에서 산것 기록 없애고 DB에 넣어야 함 (왜냐하면 환불 후 또 구입하는 경우 가능하게 하려면
+        //
+
+    }
+}
